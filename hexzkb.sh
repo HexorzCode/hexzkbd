458e486fb2e08fb28438039aa189e2a561aeb8d4
HexorzCode <107131696+HexorzCode@users.noreply.github.com>
2022-06-12 20:38:17 +0700

    Add files via upload

---
+++ hexzkb.sh
@@ -0,0 +1,111 @@
+cyan="\e[94m"
+red="\e[31m"
+purple="\e[1;95m"
+now=$(date +'%X')
+
+clear
+printf "${purple}"
+printf "                    ===============================\n"
+printf "${cyan}"
+printf "                     _   _                         \n"
+printf "                    | | | | _____  _____  _ __ ____\n"
+printf "		    | |_| |/ _ \ \/ / _ \| '__|_  /\n"
+printf "		    |  _  |  __/>  < (_) | |   / / \n"
+printf "	            |_| |_|\___/_/\_\___/|_|  /___|\n"
+echo
+printf "${purple}"
+printf "		    ===============================\n"
+echo
+sleep 0.5
+
+clear
+printf "${purple}"
+printf "                     ===============================\n"
+printf "${cyan}"
+printf "                      _   _                         \n"
+printf "                     | | | | _____  _____  _ __ ____\n"
+printf "		     | |_| |/ _ \ \/ / _ \| '__|_  /\n"
+printf "		     |  _  |  __/>  < (_) | |   / / \n"
+printf "		     |_| |_|\___/_/\_\___/|_|  /___|\n"
+echo
+printf "${purple}"
+printf "		     ===============================\n"
+echo
+sleep 0.5
+
+clear
+printf "${purple}"
+printf "                      ===============================\n"
+printf "${cyan}"
+printf "                       _   _                         \n"
+printf "                      | | | | _____  _____  _ __ ____\n"
+printf "		      | |_| |/ _ \ \/ / _ \| '__|_  /\n"
+printf "		      |  _  |  __/>  < (_) | |   / / \n"
+printf "		      |_| |_|\___/_/\_\___/|_|  /___|\n"
+echo
+printf "${purple}"
+printf "		      ===============================\n"
+echo
+sleep 0.5
+
+clear
+printf "${purple}"
+printf "                       ===============================\n"
+printf "${cyan}"
+printf "                        _   _                         \n"
+printf "                       | | | | _____  _____  _ __ ____\n"
+printf "		       | |_| |/ _ \ \/ / _ \| '__|_  /\n"
+printf "		       |  _  |  __/>  < (_) | |   / / \n"
+printf "		       |_| |_|\___/_/\_\___/|_|  /___|\n"
+echo
+printf "${purple}"
+printf "		      ===============================\n"
+echo
+sleep 0.5
+
+
+clear
+printf "${purple}"
+printf "                        ===============================\n"
+printf "${cyan}"
+printf "                         _   _                         \n"
+printf "                        | | | | _____  _____  _ __ ____\n"
+printf "			| |_| |/ _ \ \/ / _ \| '__|_  /\n"
+printf "			|  _  |  __/>  < (_) | |   / / \n"
+printf "			|_| |_|\___/_/\_\___/|_|  /___|\n"
+echo
+printf "${purple}"
+printf "			===============================\n"
+echo 
+
+
+
+
+
+
+
+
+printf "${red}"
+echo "Starting at ${now} Please Wait......"
+sleep 1.5
+echo Configure keyboard......
+sleep 2
+echo Keyboard Has Inactive
+sleep 1
+chmod +x hexzkey.sh
+./hexzkey.sh attached
+./hexzkey.sh detach
+clear
+printf "${purple}"
+printf "                        ===============================\n"
+printf "${cyan}"
+printf "                         _   _                         \n"
+printf "                        | | | | _____  _____  _ __ ____\n"
+printf "			| |_| |/ _ \ \/ / _ \| '__|_  /\n"
+printf "			|  _  |  __/>  < (_) | |   / / \n"
+printf "			|_| |_|\___/_/\_\___/|_|  /___|\n"
+echo
+printf "${purple}"
+printf "			===============================\n"
+echo 
+
